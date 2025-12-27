e126c08d6af74450291c018fb90779af2af37e28
Ravidu Rathnayaka <rmrhrathnayaka@gmail.com>
2025-12-27 16:50:04 +0530

    feat(email sending):add sending job details through email for users using Resend

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,9 +1,12 @@
 import { inngest } from "@/app/utils/inngest/client";
-import { handleJobExpiration } from "@/app/utils/inngest/functions";
+import {
+  handleJobExpiration,
+  sendPeriodicJobListings,
+} from "@/app/utils/inngest/functions";
 import { serve } from "inngest/next";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [handleJobExpiration],
+  functions: [handleJobExpiration, sendPeriodicJobListings],
 });
--- app/utils/inngest/functions.ts
+++ app/utils/inngest/functions.ts
@@ -1,5 +1,9 @@
+import { Slice } from "lucide-react";
 import prisma from "../db";
 import { inngest } from "./client";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const handleJobExpiration = inngest.createFunction(
   { id: "job-expiration" },
@@ -19,5 +23,65 @@ export const handleJobExpiration = inngest.createFunction(
     });
 
     return { jobId, message: "Job marked as expired" };
+  }
+);
+
+export const sendPeriodicJobListings = inngest.createFunction(
+  { id: "send-job-listings" },
+  { event: "jobseeker/created" },
+  async ({ event, step }) => {
+    const { userId, email } = event.data;
+
+    const totalDays = 30;
+    const intervalDays = 2;
+    let currentDay = 0;
+
+    while (currentDay < totalDays) {
+      await step.sleep("wait-interval", `${intervalDays}d`);
+      currentDay += intervalDays;
+
+      const recentJobs = await step.run("fetch-recent-jobs", async () => {
+        return await prisma.jobPost.findMany({
+          where: { status: "ACTIVE" },
+          orderBy: {
+            createdAt: "desc",
+          },
+          take: 10,
+          include: {
+            company: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        });
+      });
+
+      if (recentJobs.length > 0) {
+        await step.run("send-email", async () => {
+          const jobListingsHTML = recentJobs
+            .map(
+              (job) =>
+                ` <div style="margin-bottom:20px; padding:15px; border:1px solid #eee; border-radius:5px">
+                  <h1 style="margin:0;">${job.jobTitle}</h1>
+                  <p style="margin:5px 0"> ${job.company.name} * ${
+                  job.location
+                }</p>
+                  <p style="margin:5px;">$${job.salaryFrom.toLocaleString()} - $${job.salaryTo.toLocaleString()}</p>
+                </div>`
+            )
+            .join("");
+
+          await resend.emails.send({
+            from: "JobsFinder <onboarding@resend.dev>",
+            to: ["rmrhrathnayaka@gmail.com"],
+            subject: "Latest Job opportunities for you",
+            html: `${jobListingsHTML}`,
+          });
+        });
+      }
+    }
+
+    return { userId, message: "Completed 30 day job listing notifications" };
   }
 );
